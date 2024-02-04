583034a4fcc9476e7b8aa87de9306091753e027b
Mohammad-coders <arahaman461@gmail.com>
2024-02-04 07:48:57 +0600

    JS part 1 task temperature convert

---
+++ JS Part1 Task/temperatureConvert.js
@@ -0,0 +1,4 @@
+// Write a function to convert temperature from Celsius to Fahrenheit.
+let celsius = 100; //degree Celcius
+let fahrenheit = celsius*(9/5)+32;
+console.log("Farenheit temperature is: ",fahrenheit,"F");
